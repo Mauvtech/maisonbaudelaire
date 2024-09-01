d94ce3957a53acae0bda5071706c0b4a15e03143
Amara <asidibe@oxyl.fr>
2024-09-01 19:21:33 +0200

    essai de placement

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -2,39 +2,37 @@ import React from "react";
 
 export function Home() {
     return (
-        <div className="relative flex flex-col items-center justify-center overflow-hidden w-screen">
-            {/* Conteneur pour superposer les images */}
-            <div className="relative 3xl:-mt-[65vh] md:-mt-[45vh]  flex w-full justify-center items-center">
-                {/* Image de fond - deux fois plus large */}
-                <img
-                    src={`${process.env.PUBLIC_URL}/thebrand.png`}
-                    alt="Brand Logo"
-                    className="w-2/3"
-                />
-                {/* Image superposée - cinq fois plus petite */}
+        <div className="relative min-h-screen w-screen overflow-hidden bg-white flex justify-center items-center">
+            {/* Conteneur principal avec une image de fond */}
+            <div
+                className="relative w-full max-w-4xl h-[70vh] md:h-[80vh] lg:h-[100vh] -mt-72 bg-center bg-cover flex flex-col items-center"
+                style={{
+                    backgroundImage: `url(${process.env.PUBLIC_URL}/thebrand.png)`, // Définir "The Brand" comme image de fond
+                }}
+            >
+                {/* Image des filles au centre */}
                 <img
                     src={`${process.env.PUBLIC_URL}/MeufTransparente.png`}
-                    alt="Overlay Image"
-                    className="absolute mt-56 w-1/3 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+                    alt="Models"
+                    className="w-3/4 h-auto mt-16 mb-[-41rem]" 
                 />
-            </div>
 
-            {/* Image en dessous alignée à gauche */}
-            <div className="w-1/6  flex justify-center -mt-[65vh] -ml-[43vw]">
-                <img
-                    src={`${process.env.PUBLIC_URL}/drop.png`}
-                    alt="Drop Image"
-                    className="w-auto h-auto"
-                />
-            </div>
+                {/* Conteneur pour "Drop" et "Sticker" alignés en dessous */}
+                <div className="flex justify-between items-center w-[120%] -ml-10 mt-8">
+                    {/* Texte "Drop" à gauche */}
+                    <img
+                        src={`${process.env.PUBLIC_URL}/drop.png`}
+                        alt="Drop Text"
+                        className="w-32 md:w-24 lg:w-40 -mt-16 ml-36"
+                    />
 
-            {/* Image en dessous alignée à gauche */}
-            <div className="w-[27vw] -mt-[35vh] flex ml-[43vw]  ">
-                <img
-                    src={`${process.env.PUBLIC_URL}/sticker.png`}
-                    alt="Drop Image"
-                    className="w-auto h-auto"
-                />
+                    {/* Sticker à droite, agrandi */}
+                    <img
+                        src={`${process.env.PUBLIC_URL}/sticker.png`}
+                        alt="Sticker"
+                        className="w-48 md:w-56 lg:w-80 ml-8"
+                    />
+                </div>
             </div>
         </div>
     );
