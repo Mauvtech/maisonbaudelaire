9b2c456d6f320359524615926aa725effda16738
Amara <amara.sidibe@centrale.centralelille.fr>
2024-10-15 13:56:37 +0200

    Finitions 6 maison baudelaire en petit 2

--- src/Pages/About/About.tsx
+++ src/Pages/About/About.tsx
@@ -12,10 +12,10 @@ export function About() {
     return (
         <div className="xl:h-screen overflow-scroll">
             <main
-                className="mx-auto flex 2xl:mt-0 xl:mt-[5%] md:w-[80%]  xl:flex-row mt-[20%] flex-col justify-between items-center px-8">
+                className="mx-auto flex 2xl:mt-0 xl:mt-[5%] md:w-[80%]  xl:flex-row mt-[10%] flex-col justify-between items-center px-8">
                 <div
                     className="xl:w-1/2 w-full gap-[2%] flex flex-col justify-center items-center lg:items-start h-full py-4">
-                    <h2 className="md:text-6xl text-3xl font-bold mb-[6%] text-error text-center">À PROPOS</h2>
+                    <h2 className="md:text-6xl text-3xl font-bold lg:mb-[6%] text-error text-center">À PROPOS</h2>
                     <AboutItem>
                         « Maison Baudelaire est née de la passion de quatre jeunes pour la mode et la culture urbaine.»
                     </AboutItem>
@@ -29,8 +29,8 @@ export function About() {
                     <AboutItem>
                         « Bienvenue chez Maison Baudelaire , ou l’authenticité et l’audace se rencontrent »
                     </AboutItem>
-                    <div className={"w-full flex justify-end"}>
-                        <p className="mt-8 font-bold text-2xl"> - MAISON BAUDELAIRE</p>
+                    <div className={"w-full flex justify-center"}>
+                        <p className="lg:mt-8 mt-2 font-bold text-lg lg:text-2xl mb-6 lg:mb-0"> MAISON BAUDELAIRE</p>
                     </div>
 
                 </div>
