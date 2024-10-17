f82dcaeb3e1d3657b3defc155706194b8497ebad
Amara <amara.sidibe@centrale.centralelille.fr>
2024-10-17 23:50:53 +0200

    sold out valider

--- src/components/StickerPopup.tsx
+++ src/components/StickerPopup.tsx
@@ -28,12 +28,12 @@ export default function StickerPopup({formData, setFormData, setOpen}: StickersP
                 }}
             >
                 <SelectionForm
-                    classValider="mr-3 -mt-4"
+                    classValider="mr-3 -mt-8"
                     className="px-0 lg:px-16 mr-6 lg:mt-0 mt-4 tracking-wider caret-white absolute left-1/4 inset-0 flex flex-col justify-center font-shadows text-[0.6rem] -translate-x-[10%] sm:text-[17px] md:text-[21px] lg:text-[26px] xl:text-[35px] 2xl:text-[36px] text-white"
                     formData={formData} setFormData={setFormData}
                     onSend={() => setOpen(false)}
                 />
-                
+
             </div>
         </div>
     )
